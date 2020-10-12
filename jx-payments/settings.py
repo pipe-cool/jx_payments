d2d94fbf19c7125e61ecc2a09fdcbafc6f6cebbe
Miguel Montalvo <pomodroizer@gmail.com>
2020-10-12 01:42:44 +0100

    PR-1 Little commit

--- jx-payments/settings.py
+++ jx-payments/settings.py
@@ -16,9 +16,6 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'su9id&)ww_kn0%^vus+e)0^z!14hu)fu$r6rs=&d_t3hx=ise7'
 
